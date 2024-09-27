c959cb2bffc68e73c01cc72a068eb5246a7785cc
xinzhouhe <danielxinzhouhe@outlook.com>
2024-09-27 00:46:57 -0400

    add api response

---
+++ __jac_gen__/server.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+from jaclang.plugin.feature import JacFeature as _Jac
+from jaclang.plugin.builtin import *
+from dataclasses import dataclass as __jac_dataclass__
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class interact(_Jac.Walker):
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, world!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class interact_with_body(_Jac.Walker):
+    name: str
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, ' + self.name + '!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class major(_Jac.Walker):
+    name: str
+    major: str
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Welcome, ' + self.name + ' from ' + self.major})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class school(_Jac.Walker):
+    name: str
+    school: str
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hi, ' + self.name + '. Welcome to ' + self.school})
